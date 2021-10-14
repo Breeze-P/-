454272cf0cb799e9c2b012049f749d84519768e9
Breeze-P <zhiyuzhao8@gmail.com>
2021-10-15 00:23:31 +0800

    backstage with show version 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './features/main/index'
+import Backstage from "./backstage";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+        <Router>
+            <Switch>
+                <Route path='/backstage'>
+                    <Backstage />
+                </Route>
+                <Route path='/'>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
--- src/features/main/index.js
+++ src/features/main/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import GamePlayer from "../gamePlayer";
+import {Link} from "react-router-dom";
 
 function Home() {
     return (
         <main className="lottery_wrapper">
             {/*<Title/>*/}
+            <div className="backstage-load">
+                <Link to='/backstage'>
+                    后台
+                </Link>
+            </div>
             <div className="game-container">
                 <GamePlayer/>
                 {/*<MessageTable/>*/}
